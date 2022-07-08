4b2a1b3e00ab59c04b36d5743a4f3aa48ed2ea71
unknown <cogjava1777@iiht.tech>
2022-07-08 14:24:26 +0530

    adding api calls useing react app

---
+++ photoshop/src/AlbumList.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+function AlbumList() {
+    let [photos, setPhotos] = useState([]); //in order to trigger a re-render , use state
+    if (!photos.length) {
+        getAlbums();
+    }
+    async function getAlbums() {
+        photos = await (await fetch("https://jsonplaceholder.typicode.com/photos?_page=1")).json();//why twice
+        console.log(photos);
+        setPhotos(photos);
+    }
+    return (
+        <div>
+            <table>
+                <thead>
+                   <tr> <td>Title</td>
+                    <td>URL</td></tr>
+                </thead>
+                <tbody>
+                    {
+                        photos.map(function (photo) {
+                            return <tr><td>{photo.title}</td>
+                                <td>{photo.url}</td></tr>
+                        })
+                    }</tbody>
+            </table>
+        </div>
+    )
+}
+
+export default AlbumList;
---
+++ photoshop/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AlbumList from './AlbumList';
+
+function App() {
+  return (
+    <div>
+      <AlbumList></AlbumList>
+    </div>
+  );
+}
+
+export default App;
