08577f445dc7b34e7377e38eee8a66dfa5f920ab
unknown <cogjava1777@iiht.tech>
2022-07-12 09:41:13 +0530

    adding day-7

--- Day-6/Web-Basics/calculator.js
+++ Day-6/Web-Basics/calculator.js
@@ -1,5 +1,5 @@
-let opr1 = document.getElementById('opr1');
-let opr2 =document.getElementById('opr2');
+var opr1 = document.getElementById('opr1');
+var opr2 =document.getElementById('opr2');
 function addition() {
     
     console.log(parseInt(opr1.value) + parseInt(opr2.value));
