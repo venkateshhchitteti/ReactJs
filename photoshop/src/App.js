7e63735827020f093c2d9c1415e2ad939eac99f0
unknown <cogjava1777@iiht.tech>
2022-07-13 11:59:12 +0530

    adding remaining code of day-8

--- photoshop/src/App.js
+++ photoshop/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import AlbumList from './AlbumList';
 import FormValidation from './FormValidation';
 import FormValidation2 from './FormValidation2';
+import Email from './Email';
 function App() {
   return (
     <div>
       {/* <AlbumList></AlbumList> */}
       <FormValidation></FormValidation>
       {/* <FormValidation2></FormValidation2> */}
+      <Email/>
     </div>
   );
 }
