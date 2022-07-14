0b1125216ddd45f704b9b83432d0eb05c68e8328
unknown <cogjava1777@iiht.tech>
2022-07-14 09:55:31 +0530

    day 9 starting code

---
+++ Day-9/demo.js
@@ -0,0 +1,9 @@
+var a = 1;
+a = 'test';
+var b;
+b = 2;
+function add(a, b) {
+    return a + b;
+}
+var result = add(2, '34');
+console.log(result);
---
+++ Day-9/demo.ts
@@ -0,0 +1,11 @@
+let a=1;
+a = 'test';
+
+let b:string;
+b=2;
+function add(a:number, b:number):numberu{
+    return a+b;
+}
+
+const result = add(2,'34');
+console.log(result);
