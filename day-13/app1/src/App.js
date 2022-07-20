b0e168cd9f0c3b074081383e00fafa0cd8497e6b
unknown <cogjava1777@iiht.tech>
2022-07-20 09:50:59 +0530

    day-13

---
+++ day-13/app1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './main';
+
+
+function App() {
+  return (
+    <div >
+      <Clock></Clock>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-13/app1/src/main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function addMinutes(date, minutes) {
+    //we multiply minutes by 60000 is to convert minutes to milliseconds
+    return new Date(date.getTime() + minutes * 60000);
+  }
+  
+  function Clock() {
+    const [time, setTime] = React.useState(new Date());
+  
+    const handleClick = () => {
+      setTime(addMinutes(time, 10));
+    };
+  
+    return (
+      <div>
+        <p>{time.toLocaleTimeString()}</p>
+        <button onClick={handleClick}>+ 10 Minutes</button>
+      </div>
+    );
+  }
+  
+ReactDOM.createRoot(document.getElementById("root")).render(<Clock />);
+
+
+export default Clock;
